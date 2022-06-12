00cf9f507de0f4388ce1205724324a320b37c1b0
jerry44000 <shai.badarna@gmail.com>
2022-06-12 21:28:23 +0200

    middlewares

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require("express");
 let pokemons = require("./mock-pokemons.js");
+const morgan = require('morgan');
+const { success } = require("./helper.js");
 const app = express();
 const port = 5000;
+
+app.use(morgan('dev'));
 
 app.get("/", (req, res) => res.send("Hello, Express3"));
 
 app.get("/api/pokemons/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const pokemon = pokemons.find((pokemon) => pokemon.id === id);
-  res.json(pokemon);
+  const message = "Pokemon found";
+  res.json(success(message, pokemon));
 });
 
 app.get("/api/pokemons", (req, res) => {
-    res.send(`There is ${pokemons.length} pokemons around`)
-})
+  const message = `Here is the list of all Pokemons. There is ${pokemons.length} pokemons around`;
+  res.json(success(message, pokemons));
+});
 
 app.listen(port, () =>
   console.log(`Listened on port http://localhost:${port}`)
