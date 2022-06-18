64ae0c740bbf1e8bc93dcba92f127939710853ad
jerry44000 <shai.badarna@gmail.com>
2022-06-18 14:44:45 +0200

    heroku

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require("express");
-const morgan = require("morgan");
 const favicon = require("serve-favicon");
 const bodyParser = require("body-parser");
 const sequelize = require("./src/db/sequelize");
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app
   .use(favicon(__dirname + "/favicon.ico"))
-  .use(morgan("dev"))
   .use(bodyParser.json());
 
 sequelize.initDb();
 
+app.get('/', (req, res) => {
+  res.json('Hello, Heroku !!')
+})
 //Points de terminaison
 
 //Trouve tout les pokemons
